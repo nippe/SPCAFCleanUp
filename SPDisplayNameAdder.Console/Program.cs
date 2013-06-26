782092f1ab2c1b9330d6872d5f132cefcb1d5cd8
Niklas Nihlen <niklas.nihlen@gmail.com>
2013-06-26 10:29:03 +0200

    Removes ReadOnly flag if user approves on files. Still not working

--- SPDisplayNameAdder.Console/Program.cs
+++ SPDisplayNameAdder.Console/Program.cs
@@ -10,6 +10,10 @@ namespace SPDisplayNameAdder
 {
   class Program
   {
+
+    static bool yesToAll = false;
+
+
     static void Main(string[] args)
     {
       if(args.Length != 1) {
@@ -19,12 +23,8 @@ namespace SPDisplayNameAdder
       }
 
       string rootPath = args[0];
-
       DirectoryInfo rootFolder = new DirectoryInfo(rootPath);
-
       TraverseFoler(rootFolder);
-
-
     }
 
     private static void TraverseFoler(DirectoryInfo currentFolder) {
@@ -34,18 +34,40 @@ namespace SPDisplayNameAdder
     }
 
     private static void AddDisplayNameToElements(string fileName) {
-      if(File.GetAttributes(fileName).HasFlag(FileAttributes.ReadOnly)) {
-        Console.WriteLine("{0} is ReadOnly");
-        if(Console.ReadLine() == "y") {
-          File.SetAttributes(fileName, File.GetAttributes((fileName) & ~FileAttributes.ReadOnly));
+      AskAndUnlockFileIfReadOnlyFlag(fileName);
+      bool change = false;
+
+      XDocument doc = XDocument.Load(fileName);
+      IEnumerable<XElement> fieldRefs = doc.Descendants("FieldRef");
+      
+      foreach (XElement fieldRef in fieldRefs) {
+        if(fieldRef.Attribute("DisplayName") == null) {
+          fieldRef.Add(new XAttribute("DisplayName", fieldRef.Attribute("Name").Value));
+          change = true;
         }
       }
-      XDocument doc = XDocument.Load(fileName);
-      IEnumerable<XElement> fieldRefs = doc.Document.Descendants("FieldRef");
-      foreach (XElement fieldRef in fieldRefs) {
-        fieldRef.Add(new XAttribute("DisplayName", fieldRef.Attribute("Name").Value));
+
+      if (change) {
+        doc.Save(fileName, SaveOptions.None);
       }
-      doc.Save(fileName, SaveOptions.None);
+    }
+
+    private static void AskAndUnlockFileIfReadOnlyFlag(string fileName) {
+      if (File.GetAttributes(fileName).HasFlag(FileAttributes.ReadOnly)) {
+        if (!yesToAll) {
+          Console.WriteLine("{0} is ReadOnly. Remove ReadOnly flag? [A]ll, [Y]es to this file or [N]o:", fileName);
+          ConsoleKeyInfo answer = Console.ReadKey();
+          if (answer.KeyChar == 'y' || answer.KeyChar == 'a') {
+            if (answer.KeyChar == 'a') {
+              yesToAll = true;
+            }
+            File.SetAttributes(fileName, File.GetAttributes(fileName) & ~FileAttributes.ReadOnly);
+          }
+        }
+        else {
+          File.SetAttributes(fileName, File.GetAttributes(fileName) & ~FileAttributes.ReadOnly);          
+        }
+      }
     }
   }
 }
